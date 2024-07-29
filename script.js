15829b6bd7bc03da1a89fa6f551ee2e683cbf9d9
Ricardo <MPACHERRERICARD@uss.edu.pe>
2024-07-29 17:49:46 -0500

    hola ALexander

---
+++ script.js
@@ -0,0 +1,5 @@
+function selectLevel(level) {
+    alert('Has seleccionado el Nivel ' + level);
+    // Aquí puedes añadir el código para cargar el nivel seleccionado
+    // Por ejemplo, redirigir a otra página o cambiar el contenido dinámicamente
+}
