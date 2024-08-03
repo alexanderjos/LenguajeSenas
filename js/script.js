0f6524385a77d8998f3059b1f8a9464f89462553
Alexander Valverde <alexandervalverdecastillo83@gmail.com>
2024-08-03 00:54:28 -0500

    cambios

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Maneja el evento de clic en el botón de salir
-document.getElementById('exitButton').addEventListener('click', function(event) {
+document.getElementById('exitButton')?.addEventListener('click', function(event) {
     event.preventDefault();
     Swal.fire({
         title: '¿Estás seguro de que quieres salir del juego?',
@@ -14,3 +14,15 @@ document.getElementById('exitButton').addEventListener('click', function(event) 
     });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+
+    if (menuToggle && menu) {
+        menuToggle.addEventListener('click', () => {
+            menu.style.left = (menu.style.left === '0px') ? '-300px' : '0px';
+        });
+    } else {
+        console.error('Elementos de menú no encontrados.');
+    }
+});
