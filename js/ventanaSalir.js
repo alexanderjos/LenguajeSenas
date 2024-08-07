9fe7b6cccf58fca75658f4ed95910edf7dad30fd
Alexander Valverde <alexandervalverdecastillo83@gmail.com>
2024-08-07 15:37:29 -0500

    cambios

---
+++ js/ventanaSalir.js
@@ -0,0 +1,15 @@
+// Maneja el evento de clic en el botón de salir
+document.getElementById('exitButton')?.addEventListener('click', function(event) {
+    event.preventDefault();
+    Swal.fire({
+        title: '¿Estás seguro de que quieres salir del juego?',
+        showCancelButton: true,
+        confirmButtonText: `Sí, salir`,
+        cancelButtonText: `No, cancelar`,
+        icon: 'warning'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = '../index.html'; // Redirigir a la página principal
+        }
+    });
+});
