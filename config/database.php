7ed3612bfaa965f2ded98e089ec29aefa6d7b041
Alexander Valverde <alexandervalverdecastillo83@gmail.com>
2024-08-09 11:26:29 -0500

    cambios

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php 
+class Conectar{
+    public static function Conectar(){
+        $conexion = new mysqli("localhost", "root","", "bd_praia");
+        if($conexion->connect_errno){
+            die("Error inesperado en la conexión a base de datos: ". $conexion->connect_errno);
+        }else{
+            print("Conectado");
+            return $conexion; 
+        }
+    }
+}
+?>
