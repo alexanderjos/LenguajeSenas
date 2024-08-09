9644386260e94c35aa90762935b8d7f82b7af235
Alexander Valverde <alexandervalverdecastillo83@gmail.com>
2024-08-09 12:13:17 -0500

    cambios

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php 
 class Conectar{
     public static function Conectar(){
-        $conexion = new mysqli("b1hatqzetyib0yxinmxa-mysql.services.clever-cloud.com", "uhknump3kjjmv2nh","KiD9F7loevq6joZrqOQE", "b1hatqzetyib0yxinmxa ");
+        $conexion = new mysqli("b1hatqzetyib0yxinmxa-mysql.services.clever-cloud.com", "uhknump3kjjmv2nh","KiD9F7loevq6joZrqOQE", "b1hatqzetyib0yxinmxa");
         if($conexion->connect_errno){
             die("Error inesperado en la conexión a base de datos: ". $conexion->connect_errno);
         }else{
